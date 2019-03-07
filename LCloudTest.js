60587b254fddf7c03ab2f04347e185d9f467cfe9
Dan Elis <danelis19@gmail.com>
2019-03-07 17:12:33 +0100

    initial commit for first task

---
+++ LCloudTest.js
@@ -0,0 +1,26 @@
+require('dotenv').config();
+
+// Load the SDK for JavaScript
+var AWS = require('aws-sdk');
+// Set the region 
+AWS.config.update({ region: 'eu-west-1' });
+
+// Create S3 service object
+s3 = new AWS.S3({ apiVersion: '2006-03-01' });
+
+// Testing to see LCloud AWS Key Id
+console.log(process.env.AWS_ACCESS_KEY_ID)
+
+// Create the parameters for calling listObjects
+var bucketParams = {
+  Bucket: process.env.AWS_ACCESS_KEY_ID,
+};
+
+// Call S3 to obtain a list of the objects in the bucket
+s3.listObjects(bucketParams, function (err, data) {
+  if (err) {
+    console.log("Error", err);
+  } else {
+    console.log("Success", data);
+  }
+});
